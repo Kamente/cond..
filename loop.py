2fa61817c3363b6ed8ee75f083522530a5d29d4c
Kamente <justinm48000@gmail.com>
2023-08-10 05:54:32 +0300

    .

---
+++ loop.py
@@ -0,0 +1,12 @@
+messages = [
+    "Wait for real?",
+    "I am for real though i need this",
+    "So you ain't gonna give up on us?",
+    "Last chance please be on your way",
+    "Man I Love you",
+]
+message_index = 0
+while True:
+    user_input = input("Do you want to break up?: (yes/no): ").lower()
+    
+    
