8c077f9a4834f56d9fa4bb0ea35876f237d0ef8b
Kamente <justinm48000@gmail.com>
2023-08-10 06:17:18 +0300

    conditions

--- loop.py
+++ loop.py
@@ -1,12 +1,18 @@
 messages = [
-    "Wait for real?",
-    "I am for real though i need this",
-    "So you ain't gonna give up on us?",
-    "Last chance please be on your way",
-    "Man I Love you",
+    "Incorrect",
+    "Really?",
+    "Wait, How Don't you Know?"
+    "You know what, just forget about it"
 ]
+
 message_index = 0
+
 while True:
-    user_input = input("Do you want to break up?: (yes/no): ").lower()
+    user_input = input("What's my favorite sport? :").lower()
     
-    
+    if user_input == "basketball":
+        print("How did you know?")
+        break
+    else:
+        print(messages[message_index])
+        message_index = (message_index + 1) % len(messages)
